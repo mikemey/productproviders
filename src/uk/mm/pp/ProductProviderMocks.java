36cc1c7c8863aca22f98ac8b7572e95611cd885c
mikemey <miguel.maestro@gmx.co.uk>
2016-02-27 19:01:08 +0000

    multiple products in response, randomised image + description.

--- src/uk/mm/pp/ProductProviderMocks.java
+++ src/uk/mm/pp/ProductProviderMocks.java
@@ -3,13 +3,21 @@ package uk.mm.pp;
 import com.github.dreamhead.moco.HttpServer;
 import com.github.dreamhead.moco.Moco;
 import com.github.dreamhead.moco.Runner;
+import com.google.common.base.Joiner;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import static com.github.dreamhead.moco.Moco.by;
 import static com.github.dreamhead.moco.Moco.header;
 import static com.github.dreamhead.moco.Moco.latency;
+import static com.github.dreamhead.moco.Moco.log;
 import static com.github.dreamhead.moco.Moco.text;
 import static com.github.dreamhead.moco.Moco.uri;
 import static com.github.dreamhead.moco.Moco.with;
+import static java.nio.charset.Charset.defaultCharset;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.commons.lang3.RandomUtils.nextInt;
@@ -18,8 +26,10 @@ import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 
 public class ProductProviderMocks {
-    private final static String LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    private static final int DESCRIPTION_LEN = 200;
     private final static String OFFER_URL = "http://www.google.com";
+    private final static String DESCRIPTIONS = readRandomFile();
+
     private final static String[] IMAGE_URLS = {
             "http://www.credit-card-logos.com/images/multiple_credit-card-logos-2/credit_card_logos_29.gif",
             "http://www.credit-card-logos.com/images/visa_credit-card-logos/visa_infinite_chip2.gif",
@@ -31,7 +41,16 @@ public class ProductProviderMocks {
             "http://www.credit-card-logos.com/images/discover_credit-card-logos/discover_network1.jpg"
     };
 
-    public static void main(String[] args) {
+    private static String readRandomFile() {
+        try {
+            List<String> lines = Files.readLines(new File("resources/random.txt"), defaultCharset());
+            return Joiner.on(' ').join(lines);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         startProductServer(2016, 1.5);
         startProductServer(2017, 2.5);
         startProductServer(2018, 7.5);
@@ -41,12 +60,12 @@ public class ProductProviderMocks {
         startProductServer(2021, 3.5);
 
         startProductServer(2022, 2.8);
-        startProductServer(2023, 4.5);
-        startProductServer(2024, 5.3);
+        startProductServer(2023, 5.0);
+        startProductServer(2024, 6.3);
     }
 
     private static void startProductServer(int port, double delay) {
-        HttpServer server = Moco.httpServer(port);
+        HttpServer server = Moco.httpServer(port, log());
         server.get(by(uri("/3rd/products")))
                 .response(
                         header(CONTENT_TYPE, APPLICATION_JSON.toString()),
@@ -54,10 +73,11 @@ public class ProductProviderMocks {
                         with(latency(toInt(delay), MILLISECONDS))
                 );
         Runner.runner(server).start();
+        System.out.println("started server on port: " + port);
     }
 
     private static String productRecords() {
-        int count = nextInt(0, 5);
+        int count = nextInt(0, 6);
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i < count; i++) {
@@ -73,9 +93,9 @@ public class ProductProviderMocks {
     private static String productRecord() {
         String ccName = "CCard " + randomAlphabetic(4);
         return replace("{ 'name':'" + ccName + "',\n" +
-                "  'OFFER_URL':'" + OFFER_URL + "', \n" +
-                "  'desc':'" + LOREM + "', \n" +
-                "  'imgUrl':'" + randomUrl() + "'\n" +
+                "  'offer_url':'" + OFFER_URL + "', \n" +
+                "  'desc':'" + randomText() + "', \n" +
+                "  'img_url':'" + randomUrl() + "'\n" +
                 "}", "'", "\"");
     }
 
@@ -85,5 +105,10 @@ public class ProductProviderMocks {
 
     private static String randomUrl() {
         return IMAGE_URLS[nextInt(0, IMAGE_URLS.length)];
+    }
+
+    private static String randomText() {
+        int startIx = nextInt(0, DESCRIPTIONS.length() - DESCRIPTION_LEN);
+        return DESCRIPTIONS.substring(startIx, startIx + DESCRIPTION_LEN);
     }
 }
